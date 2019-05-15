9467f8687c8dd39d4b0219038d952c597e461cd5
Fish <myps64152002@yahoo.com.tw>
2019-05-15 13:25:41 +0800

    More Learn

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/fish/PycharmProjects/Python-Flask-Learn/venv"
+set -gx VIRTUAL_ENV "/Users/fish/Documents/workspace/Python-Flask-Learn/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
